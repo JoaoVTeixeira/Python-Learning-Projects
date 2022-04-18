1f7e63aed327b10d8aa80b12491b123fca35e742
JoaoVTeixeira <103959681+JoaoVTeixeira@users.noreply.github.com>
2022-04-18 14:39:22 -0400

    Add files via upload

---
+++ pergunte_algo.py
@@ -0,0 +1,14 @@
+import random
+
+number = random.randint(1,3)
+
+if number == 1:
+    print("Yes")
+else: 
+    if number == 2:
+        print("Maybe")
+    else:
+        if number == 3:
+            print("No")
+
+print (number)
